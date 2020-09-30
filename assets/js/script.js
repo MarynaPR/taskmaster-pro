6ecca9fcfb0847e133773ef1269765822fdade5d
MarynaPR <pryadkamaryna@gmail.com>
2020-09-29 19:34:42 -0500

    calendar datepicker and highlight added

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,20 +3,25 @@ var tasks = {};
 var createTask = function (taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
-  var taskSpan = $("<span>")
-    .addClass("badge badge-primary badge-pill")
-    .text(taskDate);
-  var taskP = $("<p>")
-    .addClass("m-1")
-    .text(taskText);
+
+  var taskSpan = $("<span>").addClass("badge badge-primary badge-pill").text(taskDate);
+
+  var taskP = $("<p>").addClass("m-1").text(taskText);
 
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  // check due date
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
 };
+//adds a calendar drop menu:
+$("#modalDueDate").datepicker({
+  //to block out past dates:
+  minDate: 1
+});
 
 var loadTasks = function () {
   tasks = JSON.parse(localStorage.getItem("tasks"));
@@ -39,6 +44,29 @@ var loadTasks = function () {
       createTask(task.text, task.date, list);
     });
   });
+};
+//This functionality may be harder to test, as it is checking if the due date has passed today's date and we've explicitly set the jQuery UI Datepicker to not allow past dates to be selected. But we can always test it by removing the minDate setting from the .datepicker() methods we've used in the application; then we can put that setting back in after we test it.
+//First we utilize the jQuery .removeClass() method to remove any of these classes if they were already in place. This way, if we update the due date from yesterday to a week from now, that red background will be removed, as it will no longer be overdue.
+//The moment().isAfter(time) code inside the if statement is known as a query method in Moment.js's documentation. (Links to an external site.) This means that we can perform simple true or false checks on the date for more information about it.
+//We're using .isAfter(), which, when we read it left to right, gets the current time from moment() and checks if that value comes later than the value of the time variable.
+//Here, we're checking if the current date and time are later than the date and time we pulled from taskEl. If so, the date and time from taskEl are in the past, and we add the list-group-item-danger Bootstrap class to the entire task element. This will give it a red background, to let users know the date has passed.
+var auditTask = function (taskEl) {
+  // get date from task element
+  var date = $(taskEl).find("span").text().trim();
+
+  // convert to moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+
+  // remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  // apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  }
+  else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
 };
 
 var saveTasks = function () {
@@ -197,58 +225,46 @@ $(".list-group").on("blur", "textarea", function () {
 // due date was clicked
 $(".list-group").on("click", "span", function () {
   // get current text
-  var date = $(this)
-    .text()
-    .trim();
+  var date = $(this).text().trim();
 
   // create new input element
-  var dateInput = $("<input>")
-    .attr("type", "text")
-    .addClass("form-control")
-    .val(date);
+  var dateInput = $("<input>").attr("type", "text").addClass("form-control").val(date);
 
-  // swap out elements
   $(this).replaceWith(dateInput);
 
-  // automatically focus on new element
+  // enable jquery ui datepicker
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function () {
+      // when calendar is closed, force a "change" event on the `dateInput`
+      $(this).trigger("change");
+    }
+  });
+
+  // automatically bring up the calendar
   dateInput.trigger("focus");
 });
 
 
 //Next, we'll convert them back when the user clicks outside (i.e., when the element's blur event occurs).
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function () {
-  // get current text
-  var date = $(this)
-    .val()
-    .trim();
+//$(".list-group").on("blur", "input[type='text']", function () {
+//to edit the due date, instead of 'blur use 'change':
+$(".list-group").on("change", "input[type='text']", function () {
+  var date = $(this).val();
 
-  // get the parent ul's id attribute
-  var status = $(this)
-    .closest(".list-group")
-    .attr("id")
-    .replace("list-", "");
+  var status = $(this).closest(".list-group").attr("id").replace("list-", "");
+  var index = $(this).closest(".list-group-item").index();
 
-  // get the task's position in the list of other li elements
-  var index = $(this)
-    .closest(".list-group-item")
-    .index();
-
-  // update task in array and re-save to localstorage
   tasks[status][index].date = date;
   saveTasks();
 
-  // recreate span element with bootstrap classes
-  var taskSpan = $("<span>")
-    .addClass("badge badge-primary badge-pill")
-    .text(date);
-
-  // replace input with span element
+  var taskSpan = $("<span>").addClass("badge badge-primary badge-pill").text(date);
   $(this).replaceWith(taskSpan);
 
+  // Pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
-
-
 
 // remove all tasks
 $("#remove-tasks").on("click", function () {
